76c2b81627fbb357a5045f526f8733f48798a97c
kaiwebcode <kaifq1508@example.com>
2026-03-04 03:51:05 +0530

    Fix Issues Initialize

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma?: PrismaClient;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query", "error"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error"]
+        : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
