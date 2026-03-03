89575c0c94045b4cce1fbe8fd8d4995aaf02a1ca
kaiwebcode <kaifq1508@example.com>
2026-03-03 20:44:33 +0530

    Fix the issue in vercel Initialize

--- app/data/admin/admin-get-course.ts
+++ app/data/admin/admin-get-course.ts
@@ -1,13 +1,48 @@
-
-
 import { requireAdmin } from "./require-admin";
 import { prisma } from "@/lib/db";
 import { notFound } from "next/navigation";
+import { Prisma } from "@prisma/client";
 
-export async function adminGetCourse(courseId: string) {
+export type AdminCourseSingularType =
+  Prisma.CourseGetPayload<{
+    select: {
+      id: true;
+      title: true;
+      description: true;
+      fileKey: true;
+      price: true;
+      duration: true;
+      level: true;
+      status: true;
+      slug: true;
+      smallDescription: true;
+      category: true;
+      chapter: {
+        select: {
+          id: true;
+          title: true;
+          position: true;
+          lessons: {
+            select: {
+              id: true;
+              title: true;
+              description: true;
+              thumbnailKey: true;
+              position: true;
+              videoKey: true;
+            };
+          };
+        };
+      };
+    };
+  }>;
+
+export async function adminGetCourse(
+  courseId: string,
+): Promise<AdminCourseSingularType> {
   await requireAdmin();
 
-  if (!courseId) return notFound();
+  if (!courseId) notFound(); // ⬅️ IMPORTANT (no return)
 
   const data = await prisma.course.findUnique({
     where: { id: courseId },
@@ -24,11 +59,13 @@ export async function adminGetCourse(courseId: string) {
       smallDescription: true,
       category: true,
       chapter: {
+        orderBy: { position: "asc" },
         select: {
           id: true,
           title: true,
           position: true,
           lessons: {
+            orderBy: { position: "asc" },
             select: {
               id: true,
               title: true,
@@ -43,11 +80,7 @@ export async function adminGetCourse(courseId: string) {
     },
   });
 
-  if (!data) return notFound();
+  if (!data) notFound(); // ⬅️ IMPORTANT (no return)
 
   return data;
 }
-
-export type AdminCourseSingularType = Awaited<
-  ReturnType<typeof adminGetCourse>
->;
