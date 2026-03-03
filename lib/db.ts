09b90feb7ba51c2f05ad518125fb6d12a3a88b8d
kaiwebcode <kaifq1508@example.com>
2026-03-04 03:30:46 +0530

    Do some Changes in prisma code and generate Initialize

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,14 @@
-// import "server-only";
-
-import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
